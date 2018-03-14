5c475bd567a93476f54514cf6ced068abfd66ab1
songyingxin <18310523922@163.com>
2018-03-14 16:32:18 +0800

    修改部分注释

--- 糗事百科/baike.py
+++ 糗事百科/baike.py
@@ -3,36 +3,25 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-page = 1
-base_url = 'https://www.qiushibaike.com/text/page/'
-start_url = base_url + str(page) + '/'
+url = 'https://www.qiushibaike.com/text/page/1/'   # 要爬取的url
 
+#请求头部
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
 headers = {
     'User-Agent': user_agent
 }
 
-try:
-    response = requests.get(start_url, headers=headers)
-except requests.ConnectionError as e:
-    print('遇见网络问题')
-except requests.HTTPError as e:
-    print('HTTP请求返回不成功的状态码')
-except requests.Timeout as e:
-    print('请求超时')
-except requests.TooManyRedirects as e:
-    print('太多的重定向次数')
-except requests.exceptions.RequestException as e:
-    print('请求错误，未知异常')
-
+response = requests.get(url, headers=headers)    # 请求网页获取响应报文
 
 content = response.text
-soup = BeautifulSoup(content,'lxml')
-items = soup.find("div",id='content-left',class_='col1').find_all("div",class_=re.compile("article block untagged mb15 typs_long|hot|old"))
+soup = BeautifulSoup(content,'lxml')   # 解析HTML文本
+# 获取每个笑话，一个item代表一个笑话
+items = soup.find("div",id='content-left',class_='col1').find_all("div",
+             class_=re.compile("article block untagged mb15 typs_long|hot|old"))
 
 for item in items:
-    name = item.find("h2").string
-    text = item.find("div",class_="content").get_text()
-    laugh_num = item.find("span",class_="stats-vote").i.string
-    comment_num = item.find("a",class_="qiushi_comments").i.string
+    name = item.find("h2").string   # 获取作者名字
+    text = item.find("div",class_="content").get_text()    # 获取笑话内容
+    laugh_num = item.find("span",class_="stats-vote").i.string    # 获取点赞数
+    comment_num = item.find("a",class_="qiushi_comments").i.string   # 获取评论数
     print(name,text,laugh_num,comment_num)
--- 糗事百科/baike_升级版.py
+++ 糗事百科/baike_升级版.py
@@ -8,7 +8,6 @@ class spider_baike:
     对糗事百科进行爬取的爬虫，主要实现单一页面的爬取，没有深入递归爬取与多页面爬取
 
     Attributes:
-        page: 表示我们默认的爬取页面
         user_agent: 上文有说哦
         headers: 请求头部
         base_url: 你应该懂
@@ -18,12 +17,11 @@ class spider_baike:
         '''
         初始化变量
         '''
-        self.page = 1
         self.user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
         self.headers = {'User-Agent': self.user_agent}
         self.base_url = 'https://www.qiushibaike.com/text/page/'
 
-    def get_page(self,page=self.page):
+    def get_page(self,page=1):
         '''
         获取页面的HTML文本
         '''
@@ -47,30 +45,31 @@ class spider_baike:
             print('请求错误，未知异常')
             return None
 
-    def get_items(self,page=self.page):
+    def get_items(self,page=1):
         '''
         提取HTML文本中的item并以字符串的形式输出
         '''
-        result_items = ''
+        result_items = []
         response = self.get_page(page)
 
         content = response.text
         soup = BeautifulSoup(content,'lxml')
         items = soup.find("div",id='content-left',class_='col1').find_all("div",class_=re.compile("article block untagged mb15 typs_long|hot|old"))
         for item in items:
-            name = item.find("h2").string
+            name = item.find("h2").string)
             text = item.find("div",class_="content").get_text()
             laugh_num = item.find("span",class_="stats-vote").i.string
             comment_num = item.find("a",class_="qiushi_comments").i.string
-            result_item = str(name) + str(text) + str(laugh_num) + str(comment_num)
-            result_items += result_item
-
+            result_item = [name,text,laugh_num,comment_num]
+            result_items.append(result_item)
         return result_items
 
-def main():
-    spider = spider_baike()
-    result = spider.get_items(1)
-    print(result)
 
 if __name__ == '__main__':
-    main()
+    spider = spider_baike()
+    result = spider.get_items(1)
+    for item in result:
+        print('作者为: {}'.format(item[0]))
+        print('点赞数为：{}'.format(item[2]))
+        print('评论数为：{}'.format(item[3]))
+        print('内容为：{} \n\n\n'.format(item[1]))
